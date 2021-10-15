c9f72f378cf7b0c1923ac794c010fdd3ef58dca1
Sconeyz <77408363+Sconeyz@users.noreply.github.com>
2021-10-14 21:30:16 -0700

    Added speech

--- speechbubble.js
+++ speechbubble.js
@@ -1,7 +1,8 @@
-const fun = ["Wen moon ser?", "Wen Airdrop? ", "GM", "!vibes", "!floor"];
+const fun = ["Wen moon ser?", "Wen Airdrop? ", "GM", "!vibes", "!floor", "Looks rare", "!OG", "Super cycle", "Few", "Gib"];
 
 const educational = [
   "Satoshi Nakamoto is a fictitious name of the Bitcoin creator or creators, to this day no one knows who the actual creator is.",
   "Proof-of-stake is a method of securing a cryptocurrency network and achieving distributed consensus by showing ownership of currency.",
   "Proof-of-work is the most popular method of securing the blockchain, there are at least 480 implementations, including SHA-256 what Bitcoin uses",
+  "ERC-20 is the token standard for fungible tokens on Ethereum and ERC-721 is the token standard for non-fungible tokens (NFTs)"
 ];
