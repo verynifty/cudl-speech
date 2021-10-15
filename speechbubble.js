cffdd6f5cc9bbf38697e2e3ac9ed128acacb694c
Adam H <surfcoderepeat@protonmail.com>
2021-10-14 21:28:43 -0600

    const

--- speechbubble.js
+++ speechbubble.js
@@ -1,6 +1,6 @@
-fun = ["Wen moon ser?", "Wen Airdrop? ", "GM", "!vibes", "!floor"];
+const fun = ["Wen moon ser?", "Wen Airdrop? ", "GM", "!vibes", "!floor"];
 
-educational = [
+const educational = [
   "Satoshi Nakamoto is a fictitious name of the Bitcoin creator or creators, to this day no one knows who the actual creator is.",
   "Proof-of-stake is a method of securing a cryptocurrency network and achieving distributed consensus by showing ownership of currency.",
   "Proof-of-work is the most popular method of securing the blockchain, there are at least 480 implementations, including SHA-256 what Bitcoin uses",
